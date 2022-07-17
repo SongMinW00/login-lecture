0c4ebdbea469426c77b854ba2b7b4ba68cdda8f3
송민우 <songminwoo@songmin-uui-MacBookAir.local>
2022-07-17 21:39:12 +0900

    DOM을 이용한 HTML 객체 저장

--- app/app1.js
+++ app/app1.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 
+const home = require("./src/routes/home");
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
-
-const home = require("./src/routes/home");
 app.use(express.static(`${__dirname}/src/public`));
+
 app.use("/",home); //use: 미들웨어를 등록해주는 메서드
 
 module.exports = app;
